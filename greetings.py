46395b0962d7ab418e7e4de10179b1e235682b46
ansar <ansar.a6@outlook.com>
2026-06-26 23:36:36 +0500

    First automated script created

---
+++ greetings.py
@@ -0,0 +1,4 @@
+name = str(input('Please enter you name: '))
+space = ' '
+print('Welcome' + space + name + '! to your first automated script')
+print('This code was executed by by ./greetings.py command')
