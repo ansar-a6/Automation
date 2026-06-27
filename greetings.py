d76d80ed3da53ce5c0b34b0cf0d58282020823e1
ansar <ansar.a6@outlook.com>
2026-06-27 15:06:23 +0500

    explanation added

--- greetings.py
+++ greetings.py
@@ -4,3 +4,7 @@ name = str(input('Please enter you name: '))
 space = ' '
 print('Welcome' + space + name + '! to your first automated script')
 print('This code was executed by by ./greetings.py command')
+
+# shebands tell the os to run this script through python3.
+# chmod +x fileName will save file as executable file.
+# ./fileName on termianl will execute the file
