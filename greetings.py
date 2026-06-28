cba303b0290325f2c86375730f721b3529d0e5a7
ansar <ansar.a6@outlook.com>
2026-06-28 16:12:31 +0500

    code upgraded

--- greetings.py
+++ greetings.py
@@ -1,11 +1,24 @@
 #!/usr/bin/ven python3
 
+import os
+
 def greeting(name):
     space = ' '
     print('Welcome' + space + name + '! to your first automated script')
     print('This code was executed by by ./greetings.py command')
 
-name = str(input('Please enter you name: '))
+def name_chk(file_name):
+    if os.path.exists(file_name):
+        name = ''
+        with open(file_name) as file:
+            name = file.readline()
+    else:
+        with open(file_name, 'w') as file:
+            name = str(input('Please enter your name: '))
+            file.write(name)
+        return name
+
+name = name_chk('name.txt')
 greeting(name)
 
 
