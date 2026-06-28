54232417864ba0ae63c1587b81ea1407488cf1cc
ansar <ansar.a6@outlook.com>
2026-06-28 14:38:06 +0500

    coded upgraded

--- greetings.py
+++ greetings.py
@@ -1,9 +1,13 @@
 #!/usr/bin/ven python3
 
+def greeting(name):
+    space = ' '
+    print('Welcome' + space + name + '! to your first automated script')
+    print('This code was executed by by ./greetings.py command')
+
 name = str(input('Please enter you name: '))
-space = ' '
-print('Welcome' + space + name + '! to your first automated script')
-print('This code was executed by by ./greetings.py command')
+greeting(name)
+
 
 # shebands tell the os to run this script through python3.
 # chmod +x fileName will save file as executable file.
